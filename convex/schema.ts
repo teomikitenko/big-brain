4e96c2cfadd568c30d220ad9e9bc633addaca8eb
Yevhenii <jenmikitenko@gmail.com>
2024-08-11 17:48:08 +0300

    fix add function error in prod

--- convex/schema.ts
+++ convex/schema.ts
@@ -14,7 +14,7 @@ export default defineSchema({
     title: v.string(),
     describtion: v.string(),
     text: v.string(),
-    documentId: v.string(),
+    documentId: v.union(v.string(),v.id('files')) ,
   }).index('by_title', ['title']),
   cache: defineTable({
     key: v.string(),
