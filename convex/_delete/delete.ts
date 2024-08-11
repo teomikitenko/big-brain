643fd8f52e830cd48199d4aeced1d344d3a56392
Yevhenii <jenmikitenko@gmail.com>
2024-08-11 21:29:07 +0300

    fix problems with convex functions

--- convex/_delete/delete.ts
+++ convex/_delete/delete.ts
@@ -4,7 +4,8 @@ import { v } from 'convex/values';
 export const deleteFile = mutation({
   args: { id: v.id('files'), documentId: v.id('documents') },
   handler: async (ctx, { id, documentId }) => {
-    Promise.all([await ctx.db.delete(id), await ctx.db.delete(documentId)]);
+    await ctx.db.delete(id);
+    await ctx.db.delete(documentId);
   },
 });
 export const deleteNote = mutation({
