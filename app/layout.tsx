cf91cb59749696ec721ddedde1dc597a440451b9
Yevhenii <jenmikitenko@gmail.com>
2024-08-15 16:46:28 +0300

    modify opengraph object

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Header from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
+
 export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
